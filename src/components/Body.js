5e8afa43f55cef26f1217ef7d91c15a41ccb256b
Azure <marleyalmere@gmail.com>
2023-02-18 13:41:11 +0100

    todo is completed mobile and desktop
    only need to add little changes, but its functional.

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,7 +6,6 @@ export default function Body ({mode, isMobile, modeHandle}) {
     const [inputValue, setInputValue] = useState('');
     const [filterOption, setFilterOption] = useState('all');
     const [todoList, setTodoList] = useState([
-      { todo:"Add a todo List", all: true, active: false, completed: false, id:0 },
     ]);
     
 
@@ -32,7 +31,7 @@ export default function Body ({mode, isMobile, modeHandle}) {
     function toggleCompletion (id) {
         const updatedList = todoList.map(item => {
             if (item.id === id) {
-                return {...item, completed: !item.completed, active:false, all:false}
+                return {...item, completed: !item.completed}
             } 
             return item
         });
@@ -54,10 +53,10 @@ export default function Body ({mode, isMobile, modeHandle}) {
     return (
         <div className="main" style={{backgroundColor:mode?"#171823":"#E3E4F1"}}>
             <div className='main2'>
-                <div className="inputDisplay" style={{backgroundColor:mode?"#25273D":"white", width:"327px", height:"48px"}}>
+                <div className="inputDisplay" style={{backgroundColor:mode?"#25273D":"white",}}>
                     <div className="circle" onClick={handleSubmit}></div>
                     <form onSubmit={handleSubmit}>
-                    <input type="text" value={inputValue} onChange={handleInputChange} maxLength={isMobile?"35":null} style={{width:isMobile?"250px":null, backgroundColor:mode?"#25273D":"white", color:mode?"#C8CBE7":'black'}} />
+                    <input type="text" value={inputValue} placeholder="Create a new todo..." onChange={handleInputChange} maxLength={isMobile?"35":"39"} style={{width:isMobile?"250px":"350px", backgroundColor:mode?"#25273D":"white", color:mode?"#C8CBE7":'black'}} />
                     </form>
                 </div>
                 <div className="todo">
@@ -66,30 +65,41 @@ export default function Body ({mode, isMobile, modeHandle}) {
                         .filter(item => filterOption === 'all' || (filterOption === 'active' && !item.completed) || (filterOption === 'completed' && item.completed))
                         .map((todolists =>
                         <>
-                            <div key={todolists.id} className="list" style={{backgroundColor:mode?"#25273D":"white"}}>
+                            <div key={todolists.id} className="list" style={{backgroundColor:mode?"#25273D":"white"} }>
                                 <div className='circleAtodo'>
-                                    <div className="circle" onClick={()=> toggleCompletion(todolists.id)}>
+                                    <div className="circle" onClick={()=> toggleCompletion(todolists.id)}   >
                                         {todolists.completed ? <img src={check} alt="check"/>:null}
                                     </div>
-                                    <div className='todoList' style={{color:mode?"#C8CBE7":'black'}} onClick={()=> toggleCompletion(todolists.id)}>{todolists.todo}</div>
+                                    <div className='todoList' style={{cursor:"pointer", color:mode?"#C8CBE7":'black', textDecoration:todolists.completed?"line-through":null }} onClick={()=> toggleCompletion(todolists.id)}>{todolists.todo}</div>
                                 </div> 
-                                    <img src={deleteImg} alt="delete" onClick={() => deleteTodoItem(todolists.id)} style={{width:isMobile?"11.79px":null,height:isMobile?"11.79px":null, marginRight:isMobile?"20.21px":null}} /> 
+                                    <img src={deleteImg} alt="delete" onClick={() => deleteTodoItem(todolists.id)} style={{cursor:"pointer", width:isMobile?"11.79px":null,height:isMobile?"11.79px":"18px", marginRight:isMobile?"20.21px":"24px"}} /> 
                                 </div>
 
                         </>
                     ))}
                     </>
                     <div className='counterClear' style={{backgroundColor:mode?"#25273D":"white"}}>
-                            <p style={{marginLeft:isMobile?"20px":null}} >        
+                            <p style={{marginLeft:isMobile?"20px":"24px", }} >        
                             {todoList.filter(item => filterOption === 'all' || (filterOption === 'active' && !item.completed) || (filterOption === 'completed' && item.completed)).length} items left</p>
-                            <p onClick={clearCompletion} style={{marginRight:isMobile?"20px":null}}>Clear Completed</p>
+                            <>
+                                {!isMobile ? (
+                                    <div className='options' style={{backgroundColor:mode?"#25273D":"white", width:"166px"}}>
+                                        <p style={{color:filterOption === 'all' ? "#3A7CFD" : null}} onClick={() => setFilterOption('all')}>All</p>
+                                        <p style={{color:filterOption === 'active' ? "#3A7CFD" : null}} onClick={() => setFilterOption('active')}>Active</p>
+                                        <p style={{color:filterOption === 'completed' ? "#3A7CFD" : null}} onClick={() => setFilterOption('completed')}>Completed</p>
+                                    </div>
+                                ):null}
+                            </>
+                            <p className='pointer' onClick={clearCompletion} style={{marginRight:isMobile?"20px":null}}>Clear Completed</p>
                     </div>
-
-                    <div className='options' style={{backgroundColor:mode?"#25273D":"white"}}>
-                        <p style={{color:filterOption === 'all' ? "#3A7CFD" : null}} onClick={() => setFilterOption('all')}>All</p>
-                        <p style={{color:filterOption === 'active' ? "#3A7CFD" : null}} onClick={() => setFilterOption('active')}>Active</p>
-                        <p style={{color:filterOption === 'completed' ? "#3A7CFD" : null}} onClick={() => setFilterOption('completed')}>Completed</p>
-                    </div>
+                    {isMobile ? (
+                        <div className='options' style={{backgroundColor:mode?"#25273D":"white"}}>
+                            <p className='pointer' style={{color:filterOption === 'all' ? "#3A7CFD" : null}} onClick={() => setFilterOption('all')}>All</p>
+                            <p className='pointer' style={{color:filterOption === 'active' ? "#3A7CFD" : null}} onClick={() => setFilterOption('active')}>Active</p>
+                            <p className='pointer' style={{color:filterOption === 'completed' ? "#3A7CFD" : null}} onClick={() => setFilterOption('completed')}>Completed</p>
+                        </div>
+                    ):null}
+                    
 
 
                 </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import mobileDarkBg from "./assets/bg-mobile-dark.jpg"
 import mobileLightBg from "./assets/bg-mobile-light.jpg"
 import mobileDarkMode from "./assets/icon-sun.svg"
 import mobileLightMode from "./assets/icon-moon.svg"
+import darkModeBg from "./assets/bg-desktop-dark.jpg"
+import lightModeBg from "./assets/bg-desktop-light.jpg"
 
 export default function Header ({mode, isMobile, modeHandle}) {
     return (
@@ -14,7 +16,15 @@ export default function Header ({mode, isMobile, modeHandle}) {
                         <img style={{width:"20px", height:"20px"}} className="mode" onClick={modeHandle} src={mode?mobileDarkMode:mobileLightMode} alt="background"/>
                     </div>
                 </> 
-            ):null}
+            ):(
+                <div className="laptopHeader" style={{marginBottom:"48px",}}>
+                    <img className="background" src={mode?darkModeBg:lightModeBg} alt="Background"/>     
+                    <div className="titleMode">
+                        <h1 style={{marginRight:"348px", color:"white"}}>T O D O</h1>
+                        <img style={{width:"26px", height:"26px", }} className="mode" onClick={modeHandle} src={mode?mobileDarkMode:mobileLightMode} alt="background"/>
+                    </div>
+                </div> 
+            )}
         </>    
     )
 }
